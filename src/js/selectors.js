194c1b46f58eccab9c237b79c682efa20fad157b
Steve Gilberd <steve@erayd.net>
2025-04-21 17:27:43 +1200

    Organise selectors

--- src/js/selectors.js
+++ src/js/selectors.js
@@ -5,15 +5,23 @@
  * @since 0.1.2
  */
 export const targetSelectors = [
+    // type: login
+    { selector: "input[autocomplete~=username i]", type: "login" },
+    { selector: "input[autocomplete~=email i]", type: "login" },
     { selector: "input[type=email i]", type: "login" },
     { selector: "input[name^=user i]", type: "login" },
     { selector: "input[name^=tel i]", type: "login" },
-    { selector: "input[autocomplete~=username i]", type: "login" },
-    { selector: "input[autocomplete~=email i]", type: "login" },
     { selector: "input[name$=username i]", type: "login" },
-    { selector: "input[type=password i]", type: "secret" },
+    { selector: "input[id$=name i]", type: "login" },
+
+    // type: secret
     { selector: "input[autocomplete~=current-password i]", type: "secret" },
-    { selector: "input[example=blacklist i]", type: "blacklist", host: ["example.com", "example.org"] },
+    { selector: "input[type=password i]", type: "secret" },
+
+    // type: totp
     { selector: "input[autocomplete~=one-time-code i]", type: "totp" },
     { selector: "input[name$=otp i]", type: "totp" },
+
+    // type: blacklist
+    { selector: "input[example=blacklist i]", type: "blacklist", host: ["example.com", "example.org"] },
 ];
