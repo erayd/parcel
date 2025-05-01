dfba0263e3e15e86820d63f8073bbf763570011a
Steve Gilberd <steve@erayd.net>
2025-05-01 23:16:40 +1200

    More comprehensive selectors

--- src/js/selectors.js
+++ src/js/selectors.js
@@ -4,19 +4,20 @@
  * List of valid targets.
  * @since 1.0.0
  */
-export const targetSelectors = [
+export var targetSelectors = [
     // type: login
     { selector: "input[autocomplete~=username i]", type: "login" },
     { selector: "input[autocomplete~=email i]", type: "login" },
     { selector: "input[type=email i]", type: "login" },
-    { selector: "input[name^=user i]", type: "login" },
-    { selector: "input[name^=tel i]", type: "login" },
-    { selector: "input[name$=username i]", type: "login" },
-    { selector: "input[id$=name i]", type: "login" },
+    { selector: "input[type=tel i]", type: "login" },
+    { selector: "input[type=number i]", type: "login" },
 
     // type: secret
     { selector: "input[autocomplete~=current-password i]", type: "secret" },
     { selector: "input[type=password i]", type: "secret" },
+    { selector: "input[name=password i]", type: "secret" },
+    { selector: "input[id=password i]", type: "secret" },
+    { selector: "input[class~=password i]", type: "secret" },
 
     // type: totp
     { selector: "input[autocomplete~=one-time-code i]", type: "totp" },
@@ -25,3 +26,12 @@ export const targetSelectors = [
     // type: blacklist
     { selector: "input[example=blacklist i]", type: "blacklist", host: ["example.com", "example.org"] },
 ];
+
+for (let s of ["login", "user", "username", "email", "alias"]) {
+    targetSelectors.push({ selector: `input[name*=${s} i]`, type: "login" });
+    targetSelectors.push({ selector: `input[id*=${s} i]`, type: "login" });
+    targetSelectors.push({ selector: `input[class*=${s} i]`, type: "login" });
+    targetSelectors.push({ selector: `input[placeholder*=${s} i]`, type: "login" });
+    targetSelectors.push({ selector: `input[title*=${s} i]`, type: "login" });
+    targetSelectors.push({ selector: `input[aria-label*=${s} i]`, type: "login" });
+}
